ef545d653ceea95daf52d59c47baf3b2f4f1d112
Nardos Tessema <nardos.tessema@gmail.com>
2017-10-16 11:03:55 -0500

    Initial commit

---
+++ src/main/java/ork/akaki/journal/task/EJournalMicroservicesApplication.java
@@ -0,0 +1,31 @@
+package ork.akaki.journal.task;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.task.configuration.EnableTask;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+@EnableTask
+public class EJournalMicroservicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EJournalMicroservicesApplication.class, args);
+	}
+	
+	@Bean
+	public SubscriberRegistrationTask SubscriberRegistrationTask() {
+		return new SubscriberRegistrationTask();
+	}
+	
+	public class SubscriberRegistrationTask implements CommandLineRunner {
+		
+		@Override
+		public void run(String ... strings) throws Exception {
+			if(strings != null) {
+				System.out.println("Subscriber: " + strings);
+			}
+		}
+	}
+}
---
+++ src/test/java/ork/akaki/journal/task/EJournalMicroservicesApplicationTests.java
@@ -0,0 +1,16 @@
+package ork.akaki.journal.task;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EJournalMicroservicesApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
